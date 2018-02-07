ad44c7cd41e161860023aa572cec1714a51b2ab3
Shane Charles <shanecharles@burningicesolutions.com>
2018-02-06 22:26:03 -0600

    day 16 part 2 with extras

--- day16.fsx
+++ day16.fsx
@@ -1,10 +1,30 @@
+#r "packages/FParsec/lib/net40-client/FParsecCS.dll"
+#r "packages/FParsec/lib/net40-client/FParsec.dll"
+open FParsec
+
+type Operation =
+    | Spin of int
+    | Exchange of int * int
+    | Partner of char * char
+
+type ParserState = unit
+let pspin : Parser<Operation,ParserState> = pchar 's' >>. pint32 |>> Spin
+
+let pexchange = pchar 'x' >>. tuple2 (pint32 .>> pchar '/') (pint32) |>> Exchange
+let ppartner = pchar 'p' >>. tuple2 (asciiLetter .>> pchar '/') (asciiLetter) |>> Partner
+
+let parseOperation = pspin <|> pexchange <|> ppartner
+
+let parseOperations = run (sepBy parseOperation (pstring ","))
+let sample = "po/k,x4/0,s12,x7/6"
+//let pexchange = pchar 'x' >>. tuple2 
+
 let spin x (programs : char []) =
     let index = programs.Length - x
     programs.[0..index-1] |> Array.append (programs.[index..])
 
 let exchange x y (programs : char []) =
-    //let ps = Array.copy programs
-    let ps = programs
+    let ps = Array.copy programs
     let p = ps.[x]
     ps.[x] <- ps.[y]
     ps.[y] <- p
@@ -22,39 +42,88 @@ let (|RegexMatch|_|) pattern input =
     else None
 
 
-let parseOperation operation = operation |> function
-    | RegexMatch "(\w)(\d*)" ["s"; n]            -> spin (n |> int)
-    | RegexMatch "(\w)(\d*)/(\d*)" ["x"; n1; n2] -> exchange (n1 |> int) (n2 |> int)
-    | RegexMatch "(\w)(\w)/(\w)" ["p"; p1; p2]   -> partner (p1.[0]) (p2.[0])
-    | _                                          -> operation |> sprintf "Unknown: %s" |> failwith 
+let parseOperation' operation = operation |> function
+    | RegexMatch "(\w)(\d*)" ["s"; n]    -> Spin (n |> int)
+    | RegexMatch "x(\d*)/(\d*)" [n1; n2] -> Exchange ((n1 |> int), (n2 |> int))
+    | RegexMatch "p(\w)/(\w)" [p1; p2]   -> Partner ((p1.[0]), (p2.[0]))
+    | _                                  -> operation |> sprintf "Unknown: %s" |> failwith 
 
 let operations = System.IO.File.ReadAllText(__SOURCE_DIRECTORY__ + "/inputs/day16.txt").Split([|','|])
-                 |> Seq.map parseOperation
+                 |> Seq.map parseOperation'
                  |> Seq.toArray
 
+
+let apply x f = f x
+
+let operation = function
+    | Spin x         -> spin x
+    | Exchange (x,y) -> exchange x y
+    | Partner (x,y)  -> partner x y
+    
+
 let execute programs = 
-    operations |> Seq.fold (fun item f -> f item) programs
+    Seq.fold (fun item op -> op |> operation |> apply item) programs
   
-let part1 = execute [|'a' .. 'p'|] |> System.String
-"abcdefghijklmnop"
-"giadhmkpcnbfjelo"
+let part1 = execute [|'a' .. 'p'|] operations |> System.String
 
-let mapMutations (ouput : char []) = Array.map (fun c -> ouput |> Array.findIndex ((=)c))
-let shortcut mapping input = 
-    let output = Array.copy input
-    mapping 
-        |> Array.iteri (fun i oi -> output.[oi] <- input.[i]) 
-    output
+let findCycle programs ops =
+    let hash = System.Collections.Generic.HashSet<string> ()
+    let rec loop (ps : char []) x =
+        seq { if ps |> System.String |> hash.Add |> not
+              then yield x
 
-[|'a' .. 'p'|]
-  |> exchange 0 2
-  |> exchange 1 10
-  |> exchange 2 8
-  |> System.String
+              let ps' = execute ps ops 
+              yield! loop ps' (x+1)
+        }
+    loop programs 0
 
 
-let part2 = 
-    let mapping = [|'a' .. 'p'|] |> mapMutations (part1 |> Seq.toArray)
-    let short = shortcut mapping
-    Seq.fold (fun acc _ -> short acc) [|'a'..'p'|] {1 .. 40}
-      |> System.String
+let iterate ops programs = 
+    let f state = execute (state |> Seq.toArray) ops
+    Seq.unfold  (f >> (fun r -> Some (r, r))) (programs)
+
+let part2Cycle ops programs = 
+    let cycle = findCycle programs ops |> Seq.head
+    let rem = 1000000000 % cycle
+    programs |> iterate operations |> Seq.skip (rem - 1) |> Seq.head |> System.String
+
+
+let part2Memoized ops programs = 
+    let dict = System.Collections.Generic.Dictionary<string,string> ()
+    let f state = if state |> dict.ContainsKey  
+                  then dict.[state]
+                  else
+                      let r = execute (state |> Seq.toArray) ops |> System.String
+                      dict.[state] <- r
+                      r
+    Seq.unfold  (f >> (fun r -> Some (r, r))) (programs)
+
+let part2MemoizedCycle ops (programs : System.String) =
+    let dict = System.Collections.Generic.Dictionary<string,string> ()
+    
+    let rec loop ps x =
+        seq { if ps |> dict.ContainsKey
+              then yield x
+
+              let ps' = execute (ps |> Seq.toArray) ops |> System.String
+              dict.[ps] <- ps'
+              yield! loop ps' (x+1)
+        }
+        
+    let cycle = loop programs 0 |> Seq.head
+    let rem = 1000000000 % cycle
+
+    let f state = if state |> dict.ContainsKey  
+                  then dict.[state]
+                  else
+                      let r = execute (state |> Seq.toArray) ops |> System.String
+                      dict.[state] <- r
+                      r
+
+    Seq.unfold  (f >> (fun r -> Some (r, r))) (programs)
+      |> Seq.skip (rem - 1) 
+      |> Seq.head
+
+#time
+let part2 = [|'a' .. 'p'|] |> part2Cycle operations
+let part2' = [|'a' .. 'p'|] |> System.String |> part2MemoizedCycle operations
